30e9bd81249675ea8ae7c4be09b56f26daae142c
arpita009 <arpitabehura.009@gmail.com>
2022-01-16 19:33:00 +0100

    Successful GET response from the server

--- src/apis/calendar/calendarAPI.js
+++ src/apis/calendar/calendarAPI.js
@@ -1,7 +1,12 @@
 // responsible for calendar-related operations
 import axios from "axios";
 
-export default function postOpen(day) {
+export function postOpen(day) {
     return axios.post(`http://localhost:5001/open/chocolate`,{day})
-        .then((response)=> response.catch((err)=>console.log('error',err)));
+        .then((response)=> response).catch((err)=>console.log('error',err));
 };
+
+export function getStatus(){
+    return axios.get(`http://localhost:5001/chocolates`)
+        .then((response)=> response).catch((err)=>console.log('error',err));
+}
--- src/components/pages/calendar/calendarSlice.js
+++ src/components/pages/calendar/calendarSlice.js
@@ -1,7 +1,8 @@
 // create calendar actions based on reducer functions
 
 import {  createSlice,createAsyncThunk } from '@reduxjs/toolkit';
-import postOpen from '../../../apis/calendar/calendarAPI';
+import Swal from 'sweetalert2';
+import {postOpen,getStatus} from '../../../apis/calendar/calendarAPI';
 
 export const calendarStatus={NotAvailable:0, Available:1, Open:2, Eaten:3};
 const initialState = {
@@ -9,11 +10,31 @@ const initialState = {
     calStatus: 'idle',
 };
 
+// export const getCalendarStatus = createAsyncThunk(
+//     'calendar/getStatus',
+//     async(day) =>{
+//         const response=await getStatus(day);
+//         console.log('response',response);
+//     }
+// );
+async function getCalendarStatus(day){
+    const response=await getStatus();
+    console.log('response getCalendarStatus',response,day);
+}
+
 export const setStatusAvailableToOpenAsync = createAsyncThunk(
     'calendar/postOpen',
     async (day)=>{
         const response= await postOpen(day);
-        console.log('response',response);
+        if(response.status===200){
+            const calendarStatusData=getCalendarStatus(day);
+            console.log('calendarStatusData',calendarStatusData);
+        }else{
+            Swal.fire({
+                icon : 'error',
+                title: "Can't post",
+            });
+        }
     }
 );
 export const calendarSlice=createSlice({
@@ -36,10 +57,21 @@ export const calendarSlice=createSlice({
           .addCase(setStatusAvailableToOpenAsync.pending, (state) => {
             state.status = 'loading';
           })
-          .addCase(setStatusAvailableToOpenAsync.fulfilled, (state, action) => {
+          .addCase(setStatusAvailableToOpenAsync.fulfilled, (state) => {
             state.status = 'idle';
-            state.value += action.payload;
-          });
+          })
+          .addCase(setStatusAvailableToOpenAsync.rejected, (state) => {
+            state.status = 'failed';
+          })
+        //   .addCase(getCalendarStatus.pending, (state) => {
+        //     state.status = 'loading';
+        //   })
+        //   .addCase(getCalendarStatus.fulfilled, (state) => {
+        //     state.status = 'idle';
+        //   })
+        //   .addCase(getCalendarStatus.rejected, (state) => {
+        //     state.status = 'failed';
+        //   })
     },
 });
 
