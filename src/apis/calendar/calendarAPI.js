f29fa23f22f7dd7641f3b7724daa7010c3f017d2
arpita009 <arpitabehura.009@gmail.com>
2022-01-18 11:37:44 +0100

    Eslint Warning Fix

--- src/apis/calendar/calendarAPI.js
+++ src/apis/calendar/calendarAPI.js
@@ -5,21 +5,21 @@ export function postOpen(day) {
   return axios
     .post(`http://localhost:5001/open/chocolate`, { day })
     .then((response) => response)
-    .catch((err) => alert('error in posting to open slot', err));
+    .catch((err) => err);
 }
 
 export function getStatus() {
   return axios
     .get(`http://localhost:5001/chocolates`)
     .then((response) => response)
-    .catch((err) => alert('error in getting response from the server', err));
+    .catch((err) => err);
 }
 
 export function postClosed(day) {
   return axios
     .post(`http://localhost:5001/eat/chocolate`, { day })
     .then((response) => response)
-    .catch((err) => alert('error in posting to eat chocolate', err));
+    .catch((err) => err);
 }
 
 export async function postReset() {
